8885b7aabc0d9d4ca5d7d065dd9a45cafabd60ac
alika_deluxe <alika_deluxe@outlook.com>
2019-12-07 14:32:17 +0900

    master

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Test_ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("やあ、みんな！");
         }
     }
 }
